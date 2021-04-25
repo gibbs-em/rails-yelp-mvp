220c684699fa2b89404bd36fb76c9d7649306ddd
Emily Giblett <elgiblett@gmail.com>
2021-04-25 09:17:14 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 10 fake restaurants...'
+if Rails.env.development?
+  Restaurant.destroy_all
+end
+10.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+  restaurant.save!
+end
+puts 'Finished!'
